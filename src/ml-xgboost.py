f3a6937a98256937d3d9dcd6ebf7c9f8ad72011d
ksdyb.park <ksdyb@amazon.com>
2022-11-04 22:59:35 +0900

    upate

--- src/ml-xgboost.py
+++ src/ml-xgboost.py
@@ -5,7 +5,10 @@ from xgboost import XGBRegressor
 
 # load model    
 model = XGBRegressor()
-model_name = "/var/task/lambda-with-ML-container/xgboost_wine_quality.json"    
+#model_name = "/var/task/lambda-with-ML-container/xgboost_wine_quality.json"    
+model_name = "xgboost_wine_quality.json"    
+print("model: ", model_name)
+
 model.load_model(model_name)
 
 def handler(event, context):
