83fe90813c23b10e9849d2902c005b52aaa12ccb
ksdyb.park <ksdyb@amazon.com>
2022-11-05 00:41:44 +0900

    update

--- src/ml-xgboost.py
+++ src/ml-xgboost.py
@@ -3,23 +3,25 @@ import numpy as np
 
 from xgboost import XGBRegressor
 
-# load model    
-model = XGBRegressor()
-#model_name = "/var/task/lambda-with-ML-container/xgboost_wine_quality.json"    
-model_name = "xgboost_wine_quality.json"    
-print("model: ", model_name)
+def handler(event, context):
+    #body = event["body-json"]
+    #print('body: ', body)
+    print('body: ', event)
 
-model.load_model(model_name)
+    # load model    
+    model = XGBRegressor()
+    #model_name = "/var/task/lambda-with-ML-container/xgboost_wine_quality.json"    
+    model_name = "xgboost_wine_quality.json"    
 
-def handler(event):
-    body = event["body-json"]
-    print('body: ', body)
+    print("model: ", model_name)
+
+    model.load_model(model_name)
 
     # inference
-#    results = model.predict(body)
-#    print('result:', results)
+    results = model.predict(body)
+    print('result:', results)
 
-#    return {
-#        'statusCode': 200,
-#        'body': results.tolist()
-#    }
+    return {
+        'statusCode': 200,
+        'body': results.tolist()
+    }
