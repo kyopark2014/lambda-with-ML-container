162a7a01e030d6c66ba9d3d05ce183d3f66e1915
ksdyb.park <ksdyb@amazon.com>
2022-11-05 12:44:54 +0900

    update

--- src/ml-xgboost.py
+++ src/ml-xgboost.py
@@ -1,26 +1,18 @@
 # package import
-import numpy as np
 import pandas as pd
 from xgboost import XGBRegressor
 
-def handler(event, context):
-    print('event: ', event)
+# load model    
+model = XGBRegressor()
+model_name = "xgboost_wine_quality.json"    
+model.load_model(model_name)
 
+def handler(event, context):
     body = event['body']
     print('body: ', body)
 
     values = pd.read_json((body))
     print('\nvalues: ',values)
-
-    # load model    
-    model = XGBRegressor()
-    #model_name = "/var/task/lambda-with-ML-container/xgboost_wine_quality.json"    
-    model_name = "xgboost_wine_quality.json"    
-
-    print("model: ", model_name)
-
-    model.load_model(model_name)
-    print("The Model was loaded")
 
     # inference
     results = model.predict(values)
